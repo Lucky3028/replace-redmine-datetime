246d1bde21950bbbff68e6c1d709ac85f7d44664
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-13 21:52:01 +0900

    refactor: 日時周りを共通化

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import dateTimes from './dateTime';
+
 // チケットの作成・更新を表示するPタグ
 const authorsPTag = document.querySelector('p.author');
 if (!(authorsPTag instanceof HTMLParagraphElement)) throw new Error();
@@ -7,23 +9,16 @@ const author = authorsPTag.children[0];
 if (!(author instanceof HTMLAnchorElement)) throw new Error();
 const authorInfo = [author, ' さんが'];
 
-// 作成日時
-const createdDateTimeTag = authorsPTag.children[1];
-if (!(createdDateTimeTag instanceof HTMLAnchorElement)) throw new Error();
-const createdDateTime = createdDateTimeTag.title;
-const readableCreatedDateTime = createdDateTimeTag.textContent ?? '';
-const createdDateTimeDescription = ` [${createdDateTime}] ${readableCreatedDateTime}前に追加.`;
+// 作成・更新日時をテキスト化
+// 更新日時はnullable
+const dateTimeSentences = dateTimes.map((dt) => {
+  const dateTimeTag = authorsPTag.children[dt.position];
+  if (!(dateTimeTag instanceof HTMLAnchorElement)) return '';
 
-// 更新日時
-// TODO: nullable!!!
-const updatedDateTimeTag = authorsPTag.children[2];
-if (!(updatedDateTimeTag instanceof HTMLAnchorElement)) throw new Error();
-const updatedDateTime = updatedDateTimeTag.title;
-const readableUpdatedDateTime = updatedDateTimeTag.textContent ?? '';
-const updatedDateTimeDescription = ` [${updatedDateTime}] ${readableUpdatedDateTime}前に更新.`;
+  const dateTime = dateTimeTag.title;
+  const readableDateTime = dateTimeTag.textContent ?? '';
 
-authorsPTag.replaceChildren(
-  ...authorInfo,
-  createdDateTimeDescription,
-  updatedDateTimeDescription,
-);
+  return ` [${dateTime}] ${readableDateTime}前に${dt.ja}`;
+});
+
+authorsPTag.replaceChildren(...authorInfo, ...dateTimeSentences);
